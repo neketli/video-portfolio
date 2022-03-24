f60c7fdbe32dda3274d75408591d35664fea4fb1
neketli <neketli.cspl@gmail.com>
2022-03-24 18:29:47 +0300

    Updated layout and added styles.

---
+++ app.js
@@ -0,0 +1,45 @@
+// video-slider
+const setVideoUrl = (source) => {
+  document.getElementById("video-slider").src = source;
+};
+
+// contacts-slider
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+  showSlides((slideIndex += 1));
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Основная функция сладера */
+const showSlides = (n) => {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var dots = document.getElementsByClassName("slider__nav-item");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+};
