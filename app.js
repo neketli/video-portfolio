90e64e732827554986ce984859f98860ee6bfbd6
neketli <neketli.cspl@gmail.com>
2022-03-25 22:22:58 +0300

    added full page slider

--- app.js
+++ app.js
@@ -1,3 +1,72 @@
+//page-scroll
+const pageSlider = new Swiper(".page", {
+  // Переназначаю дефолт классы
+  wrapperClass: "page__wrapper",
+  slideClass: "page__screen",
+
+  direction: "vertical",
+  slidesPerView: "auto",
+  speed: 1000,
+
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  watchOverflow: true,
+
+  // усправление
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  // навигация
+  pagination: {
+    el: ".page__pagination",
+    type: "bullets",
+    bulletClass: "page__bullet",
+    bulletActiveClass: "page__bullet-active",
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".page__scroll",
+    dragClass: "page__drag-scroll",
+    draggable: true,
+  },
+
+  init: false,
+
+  on: {
+    init: () => {
+      toggleScrollType();
+    },
+    resize: () => {
+      toggleScrollType();
+    },
+  },
+});
+
+const toggleScrollType = () => {
+  pageSlider.params.freeMode = false;
+
+  for (let index = 0; index < pageSlider.slides.length; index++) {
+    const page = pageSlider.slides[index];
+    const pageContent = page.querySelector(".screen__content");
+    if (pageContent) {
+      if (pageContent.offsetHeight > window.innerHeight) {
+        pageSlider.params.freeMode = true;
+        break;
+      }
+    }
+  }
+};
+
+pageSlider.init();
+
 // video-slider
 const setVideoUrl = (source) => {
   document.getElementById("video-slider").src = source;
@@ -11,7 +80,7 @@ const showSlides = (n) => {
     slideIndex = 0;
   }
   if (n < 0) {
-    slideIndex = slideItems.length-1;
+    slideIndex = slideItems.length - 1;
   }
   for (let i = 0; i < slideItems.length; i++) {
     slideItems[i].style.display = "none";
